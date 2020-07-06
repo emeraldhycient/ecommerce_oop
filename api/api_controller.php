280328edbc317583c737ff6439d681928b2c3a05
emeraldhycient <igwezehycient86@gmail.com>
2020-07-06 18:57:45 +0100

    working on the dashboard ui

--- api/api_controller.php
+++ api/api_controller.php
@@ -1,4 +1,9 @@
 <?php
+if(isset($_SERVER['REMOTE_ADDR']) AND ( $_SERVER['REMOTE_ADDR'] !== $_SERVER['SERVER_ADDR'] )){
+     $this->output->set_status_header(400, 'No Remote Access Allowed');
+      die(' Access Denied, Your IP: ' . $_SERVER['REMOTE_ADDR'] );
+      exit;
+     }
 
 include_once"products_api.php";
 include_once"users_api.php";
@@ -7,46 +12,6 @@ include_once"vendors_api.php";
 $productApi = new productApi;
 $usersApi = new usersApi;
 $vendorApi = new vendorsApi;
-
-if(isset($_FILES["vid"])){
-     $product =$productApi->insertProduct("","","",$_FILES["image"],"","","",$_FILES["vid"]);
-     echo json_encode($product);
-}
-
-/*$product = $productApi->fetchReview(23);
-
-if($product["status"] == "success" )
-{
-     while($row = $product["data"]->fetch_object()){
-          echo $row->review;
-     }
-}else{
-    echo json_encode($product);
-}
-
-$vendor = $vendorApi->login("email","sedfrer");
-
-if($vendor["status"] == "success" )
-{
-     while($row = $vendor["data"]->fetch_object()){
-          echo $row->email;
-     }
-}else{
-    echo json_encode($vendor);
-}
-$user = $usersApi->login("eudj","dhdimx");
-if($user["status"] == "success" )
-{
-     while($row = $vendor["data"]->fetch_object()){
-          echo $row->email;
-     }
-}else{
-    echo json_encode($user);
-}
-
-*/
-
-
 
 
 
